a67e987ef54d7142ce586569ae006a94046b9eb3
hiradag <hiradagarwal@gmail.com>
2026-03-02 21:50:48 +0530

    Initial Numpy Practice

--- basicnumpy.py
+++ basicnumpy.py
@@ -1,12 +1,20 @@
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5])
-print("Array:", arr)
 
-print("Mean:", np.mean(arr))
-print("Sum:", np.sum(arr))
+# Defining 1D array
+a= np.array([1,2,3])
+print(a)
+print(type(a))
 
-print("Shape:", arr.shape)
-print("Size:", arr.size)
-print("Data Type:", arr.dtype)
-print("Dimensions:", arr.ndim)
+#Defining 2D array
+b=np.array([[1,2,3],[4,5,6]])
+print(b)
+
+#Changing datatype by default it is integer type
+
+c= np.array([1,2,3],dtype=float)
+print(c)
+print(type(c)) 
+# using type function only the data type of elements in array changes not array data type
+print(c.dtype)
+#dtype is used to see the type of arr or arr's data type
